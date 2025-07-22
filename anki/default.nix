7a42e7ba8f8c384d52c6c5973892bf9c5a44a9fb
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-07-22 00:21:32 -0400

    fix: wrap anki to provide qt libraries

--- anki/default.nix
+++ anki/default.nix
@@ -5,8 +5,23 @@ inputs.nixpkgs.legacyPackages
   _system: pkgs:
   let
     addons = import ./addons/default.nix { inherit pkgs; };
+    anki-with-addons = pkgs.anki.withAddons addons;
   in
   {
-    default = pkgs.anki.withAddons addons;
+    default = pkgs.symlinkJoin {
+      name = "anki";
+      paths = [ anki-with-addons ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/anki \
+          --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [
+            pkgs.qt6.qtbase
+            pkgs.qt6.qtwayland
+            pkgs.qt6.qtsvg
+            pkgs.qt6.qtdeclarative
+            pkgs.libglvnd
+          ]}"
+      '';
+    };
   }
 )
