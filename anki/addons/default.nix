f14848f6cccdec9d75329c5b6f1bdb702a1b2a2a
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-11-16 21:49:48 -0500

    add webview

--- anki/addons/default.nix
+++ anki/addons/default.nix
@@ -4,9 +4,10 @@
   pkgs.ankiAddons.anki-connect
   pkgs.ankiAddons.passfail2
   (import ./more-overview-stats.nix { inherit pkgs; })
-  # (import ./webview-inspector.nix { inherit pkgs; })
+  (import ./webview-inspector.nix { inherit pkgs; })
   # (import ./recolor { inherit pkgs; })
   # (import ./review-heatmap.nix { inherit pkgs; })
   (import ./onigiri.nix { inherit pkgs; })
   # (import ./cloze-overlapper.nix { inherit pkgs; })
+  # (import ./hello.nix { inherit pkgs; })
 ]
--- anki/addons/webview-inspector.nix
+++ anki/addons/webview-inspector.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 (pkgs.anki-utils.buildAnkiAddon {
-  pname = "ankiw";
+  pname = "ankiwebview_inspector";
   version = "11-16-2025";
   src = pkgs.fetchFromGitHub {
     owner = "hikaru-y";
@@ -9,8 +9,32 @@
     rev = "f93954e485cce825bd547549127b6192788d9005";
     hash = "sha256-E6wlVDMhrmcVLezaEy7OOYX0kYWd86/IUwxoC386Llc=";
   };
-}).withConfig
-  {
-    config = {
-    };
-  }
+}).overrideAttrs
+  (_: {
+    installPhase = ''
+      runHook preInstall
+      
+      # 1. ORIGINAL COPY LOGIC from anki-utils.nix:
+      mkdir -p "$out/$installPrefix/user_files"
+      find . -mindepth 1 -maxdepth 1 | xargs -d'\n' mv -t "$out/$installPrefix/"
+      
+      # 2. FIX: Move files from nested 'src' up one level:
+      # Use mv with a wildcard to move the *contents* of src to the addon root.
+      # The `mv` command is safer here since the files are already inside the output path.
+      mv "$out/$installPrefix/src/"* "$out/$installPrefix/"
+      
+      # 3. Clean up the now-empty nested 'src' directory.
+      rmdir "$out/$installPrefix/src"
+      
+      runHook postInstall
+    '';
+
+  })
+
+# { pkgs, ... }:
+#
+# pkgs.anki-utils.buildAnkiAddon {
+#   pname = "wview";
+#   version = "1.0.0";
+#   src = ./anki21-addon-ankiwebview-inspector;
+# }
