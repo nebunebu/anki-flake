7283af69caed78e389c04567a7424808302d8eff
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-11-16 19:36:24 -0500

    webview not working

---
+++ anki/addons/webview-inspector.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+(pkgs.anki-utils.buildAnkiAddon {
+  pname = "anki21-addon-ankiwebview-inspector";
+  version = "11-16-2025";
+  src = pkgs.fetchFromGitHub {
+    owner = "hikaru-y";
+    repo = "anki21-addon-ankiwebview-inspector";
+    rev = "f93954e485cce825bd547549127b6192788d9005";
+    hash = "sha256-E6wlVDMhrmcVLezaEy7OOYX0kYWd86/IUwxoC386Llc=";
+  };
+}).withConfig
+  {
+    config = {
+    };
+  }
