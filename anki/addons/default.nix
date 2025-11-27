69b73fa202b16af845aa1afda7e457e4b44baa90
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-11-27 06:44:39 -0500

    activate other plugins that are not throwing errors

--- anki/addons/default.nix
+++ anki/addons/default.nix
@@ -4,13 +4,12 @@
   ...
 }:
 [
-  # Prebaked addons from nixpkgs
-  # pkgs.ankiAddons.adjust-sound-volume
-  # pkgs.ankiAddons.anki-connect
-  # pkgs.ankiAddons.passfail2
+  pkgs.ankiAddons.adjust-sound-volume
+  pkgs.ankiAddons.anki-connect
+  pkgs.ankiAddons.passfail2
 
   (import ./more-overview-stats.nix { inherit pkgs; })
-  # (import ./webview-inspector.nix { inherit pkgs; })
+  (import ./webview-inspector.nix { inherit pkgs; })
   # (import ./recolor { inherit pkgs; })
   # (import ./onigiri.nix { inherit pkgs; })
   # (import ./cloze-overlapper.nix { inherit pkgs; })
